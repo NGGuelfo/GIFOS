388aabf82fedad2c4c0fab89aea8dd24095d9184
Nicolas <guelfo26@gmail.com>
2020-08-06 14:50:32 -0300

    unificar trends con busquedas

--- scripts/busqueda.js
+++ scripts/busqueda.js
@@ -10,6 +10,10 @@ let tituloResultado = document.getElementById("tituloResultado");
 let seccionResult = document.getElementById("seccion-resultados");
 let vermas = document.getElementById("mas");
 let offset = 0;
+let trend = document.getElementById("trend");
+let apikey = "aABJW22BM12Yf086ZASgx2ZDaOkwCw0e";
+
+document.addEventListener("DOMContentLoaded", trendActuales);
 btnBuscar.addEventListener("click", buscando);
 textoBuscado.addEventListener("keyup",sugerencias);
 opciones.addEventListener("click",sugerido);
@@ -87,7 +91,31 @@ function sugerido(e){
 }
 
 function masResultados(){
-    debugger;
+
     offset = offset + 12;
     buscando();
+}
+
+function trendActuales() {
+
+
+
+    fetch(`https://api.giphy.com/v1/trending/searches?api_key=${apikey}`)
+        .then(response => response.json())
+        .then(content => {
+
+            trend.innerHTML = `
+    
+    <h3>Trending:</h3>
+
+    <h4>${content.data[0]}, ${content.data[1]}, ${content.data[2]}, ${content.data[3]}, ${content.data[4]}</h4>
+    
+    `;
+        })
+        .catch(err => {
+
+            console.log(err);
+
+        });
+
 }
